3a4dc2c9c17bfd899e26e7e34d107275a61c22aa
PaxoJW <184737673+PaxoJW@users.noreply.github.com>
2026-01-02 17:24:31 +0100

    Importing from data.js
    Created the data.js script to improt data, index.js is
    the entry point.
    Need to display everything on the page.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+import "./style.css";
+import loadData from "./data.js";
+
+
 const image = document.querySelector("img");
 let locationInput = document.querySelector("#location-input");
 const searchBtn = document.querySelector("button");
 const todayTemp = document.querySelector("#temperature");
+const daysDiv = document.querySelector("#result");
 const location1 = document.querySelector("#location-display"); //There is something preventing me from declaring location as a variable
 
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
     const loc = locationInput.value;
+    const locationInfo = loadData(loc);
+    displayData(locationInfo);
     fetchData(loc);
 })
 
@@ -29,11 +36,15 @@ function fetchData(searchWord) {
 }
 
 function displayData(r) {
-    location1.innerHTML = "";
-    todayTemp.innerHTML = "";
-
-    location1.textContent = "Location: " + r.address;
-    todayTemp.textContent = "Temperature: " + r.currentConditions.temp;
+    r.array.forEach(day => {
+        const dayDiv = document.createElement("div");
+        day.forEach(info => {
+            para = document.createElement("p");
+            para.textContent = info;
+            dayDiv.append(para);
+        })
+        daysDiv.append(dayDiv);
+    });
 }
 
 function displayGif(weatherCond) {
@@ -43,6 +54,7 @@ function displayGif(weatherCond) {
         })
         .then(function(gifResponse){
             image.src = gifResponse.data.images.downsized.url;
+            image.parentElement.classList = "visible";
             console.log(gifResponse);
         })
         .catch(function(err){
